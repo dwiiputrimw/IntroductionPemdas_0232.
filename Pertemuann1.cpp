5093162f2f4988b922d4af6a6bd01a27de3d4dfe
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-02-23 21:10:33 +0700

    Menambahkan variabel global

---
+++ Pertemuann1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//variable lokal
+int sks;
+
+int main (){
+    //variable lokal
+    int umur;
+    umur = 20; 
+
+    cout << "halo mba dwi" << endl;
+    cout << 10 << endl;
+    cout << umur;
